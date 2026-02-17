0ef53c3d65ad7e56c7e1c9034e4b331a8df08c32
dverge480 <dverge480@gmail.com>
2026-02-16 19:32:03 -0500

    Enhance secure-ssh.sh for user creation and security
    This script creates a new user, adds a public SSH key, and disables root SSH login.

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,3 +1,25 @@
-#!/bin/bash
-
 #code
+
+# secure-ssh.sh
+# author dylanverge
+# adds a public key from the local repo or curled from the remote repo 
+# removes roots ability to ssh in
+
+# Creates a user and adds the public key
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/dylan/SYS-265
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+# Blocking root ssh login
+if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+   sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+else
+   echo "PermitRootLogin not found in /etc/ssh/sshd_config"
+fi
+
+# Restart SSH
+sudo systemctl restart sshd.service
